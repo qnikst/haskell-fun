a07713675e2e49bc528af45deb116d811fa657c7
Alexander Vershilov <alexander.vershilov@gmail.com>
2015-02-11 01:09:18 +0300

    Update to match requirements

--- regions/typed-device/main.hs
+++ regions/typed-device/main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE StandaloneDeriving #-}
@@ -6,69 +7,90 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ConstraintKinds #-}
 
+import           Control.Applicative
 import           Data.Bifunctor (second)
 import           Data.IntMap (IntMap)
 import qualified Data.IntMap as Map
 import           Control.Concurrent
 import           System.IO.Unsafe
 import           Data.Typeable
+import           Data.Proxy
 
-type Index = Int
+-- Typed device index
+class MkDevice a where
+  mkDevice :: Int -> IO a
+data family DeviceIndex a :: * 
+class HaveIndex a where getIndex :: a -> Int
+-- Hack, because we can't create Typeable instances for data families:
+class ToDevRep a where 
+  type Rep a :: *
+  toDevRep :: a -> Proxy (Rep a)
+  toDevRep _ = Proxy
+instance ToDevRep a => ToDevRep (Proxy a) where
+    type Rep (Proxy a) = a
 
-data DeviceType
-        = TA
-        | TB
+-- Devices
+data DeviceA s = DeviceA {
+        deviceAI :: Int
+        } deriving Typeable
+instance MkDevice (DeviceA s) where
+  mkDevice i = return (DeviceA i)
+newtype instance DeviceIndex (DeviceA s) = IA Int
+instance HaveIndex (DeviceIndex (DeviceA s)) where getIndex (IA i) = i
+data DeviceARep = DeviceARep deriving Typeable
+instance ToDevRep (DeviceIndex (DeviceA s)) where 
+    type Rep (DeviceIndex (DeviceA s)) = DeviceARep
 
-deriving instance Typeable 'TA
-deriving instance Typeable 'TB
-
-data Device :: DeviceType -> * where
-    A :: Index -> Device TA
-    B :: Index -> Device TB
-
-deriving instance Typeable Device
-
-deviceIndex :: Device a -> Index
-deviceIndex (A i) = i
-deviceIndex (B i) = i
-
-doOnA :: Wtf s a -> SIO s ()
-doOnA (Wtf i) = SIO $ putStrLn $ "A action on " ++ show i
-
-doOnB :: Wtf s b -> SIO s ()
-doOnB (Wtf i) = SIO $ putStrLn $ "B action on " ++ show i
-
-data SomeDevice = forall a . Typeable a => SomeDevice (Device a)
-
-newtype Wtf s (a :: DeviceType) = Wtf Int
-
-newtype SIO s a = SIO {runSIO :: IO a}
+data DeviceB s = DeviceB {
+        deviceBI :: String
+        } deriving Typeable
+instance MkDevice (DeviceB s) where
+  mkDevice i = return (DeviceB (show i))
+newtype instance DeviceIndex (DeviceB s) = IB Int
+instance HaveIndex (DeviceIndex (DeviceB s)) where getIndex (IB i) = i
+data DeviceBRep = DeviceBRep deriving Typeable
+instance ToDevRep (DeviceIndex (DeviceB s)) where
+    type Rep (DeviceIndex (DeviceB s)) = DeviceBRep
 
 
-withDevice :: Typeable a => Device a -> (forall s . Wtf s a -> SIO s b) -> IO b
-withDevice d f = registering d $ \i -> runSIO $ f (Wtf i)
+type XXX g s = (ToDevRep (DeviceIndex (g s)), Typeable (Rep (DeviceIndex (g s))), HaveIndex (DeviceIndex (g s)), MkDevice (g s))
+
+data SomeIndex = forall a . Typeable a => SomeIndex a
+
+newtype SIO s a = SIO {runSIO :: IO a} deriving (Functor, Applicative, Monad)
 
 
-registering :: Typeable a => Device a -> (Int -> IO b) -> IO b
+withDevice :: XXX g s => DeviceIndex (g s) -> (forall s . g s -> SIO s b) -> IO b
+withDevice d f = registering d $ \mk -> runSIO $ mk >>= f
+
+registering :: XXX g s => DeviceIndex (g s) -> (SIO s (g s) -> IO b) -> IO b
 registering d f =
     case mv of
       Nothing -> error "register: no such device"
       Just mtx  -> 
-        withMVar mtx $ \(SomeDevice d') ->
-          if typeRep d' == typeRep d
-          then f (deviceIndex d)
+        withMVar mtx $ \(SomeIndex d') ->
+          if typeOf d' == typeRep (toDevRep d)
+          then f (SIO $ mkDevice (getIndex d))
           else error "register: type mismatch"
   where
-    mv = deviceIndex d `Map.lookup` registry
+    mv = getIndex d `Map.lookup` registry
 
-registry :: IntMap (MVar SomeDevice)
+registry :: IntMap (MVar SomeIndex)
 registry = Map.fromList 
     $ map (second (unsafePerformIO . newMVar))
-    $ [(1,SomeDevice (A 1)), (2, SomeDevice (A 2)), (3, SomeDevice (B 3))]
+    $ [(1, SomeIndex DeviceARep) , (2, SomeIndex DeviceARep), (3, SomeIndex DeviceBRep)]
 {-# NOINLINE registry #-}
 
-test1 = withDevice (A 1) $ \onDevice -> doOnA onDevice
-test2 = withDevice (B 2) $ \onDevice -> doOnA onDevice
+doOnA :: DeviceA s -> SIO s ()
+doOnA (DeviceA i) = SIO $ print i
+
+test1 = withDevice (IA 1) $ \onDevice -> doOnA onDevice
+-- Doesn't compile
+-- test2 = withDevice (IB 2) $ \onDevice -> doOnA onDevice
+
 -- Escape:
 -- test3 = withDevice (B 2) $ return
